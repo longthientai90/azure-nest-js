48d3588775da463c6a052bda7b9a73ed23915b7b
thuynt46 <longdd@scuti.asia>
2025-06-12 10:31:05 +0700

    add new

---
+++ src/items.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Post, Body, Delete, Param, Put } from '@nestjs/common';
+import { CosmosService } from './cosmos.service';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly cosmosService: CosmosService) {}
+
+  @Get()
+  findAll() {
+    return this.cosmosService.findAll();
+  }
+
+  @Post()
+  create(@Body() item) {
+    return this.cosmosService.create(item);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() item) {
+    return this.cosmosService.update(id, item);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.cosmosService.delete(id);
+  }
+}
