5436b816757f4c65cc2431ead9c2c761d76b42c6
thuynt46 <longdd@scuti.asia>
2025-07-01 09:59:22 +0700

    update

--- infa/tenant/terraform-modules/versions.tf
+++ infa/tenant/terraform-modules/versions.tf
@@ -11,13 +11,4 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  # Remote backend configuration
-  backend "azurerm" {
-    # These values should be provided via backend config file or CLI
-    # resource_group_name  = "terraform-state-rg"
-    # storage_account_name = "terraformstate"
-    # container_name       = "tfstate"
-    # key                  = "companies/{company_slug}/terraform.tfstate"
-  }
 }
