a156aac61f879933c3e2a97d09f70d597e2e5330
thuynt46 <longdd@scuti.asia>
2025-07-03 17:01:26 +0700

    update

--- infa/dify/modules/database/main.tf
+++ infa/dify/modules/database/main.tf
@@ -1,7 +1,28 @@
 # Azure Database for PostgreSQL with Private Endpoint
 
+resource "random_string" "suffix" {
+  length  = 4
+  upper   = false
+  special = false
+}
+
+resource "azurerm_subnet" "postgres_subnet" {
+  name                 = "snet-pe-db"
+  resource_group_name  = "rg-cmic"
+  virtual_network_name = "vnet-cmic"
+  address_prefixes     = ["10.1.14.0/23"]
+
+  delegation {
+    name = "postgresql-delegation"
+    service_delegation {
+      name    = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
 resource "azurerm_postgresql_flexible_server" "main" {
-  name                          = "psql-${var.environment}"
+  name                          = "psql-${var.environment}-${random_string.suffix.result}"
   resource_group_name           = var.resource_group_name
   location                      = var.location
   version                       = "13"
@@ -9,7 +30,7 @@ resource "azurerm_postgresql_flexible_server" "main" {
   administrator_password        = var.admin_password
   storage_mb                    = 32768
   sku_name                      = "B_Standard_B1ms"
-  delegated_subnet_id           = var.private_endpoint_subnet_id
+  delegated_subnet_id           = azurerm_subnet.postgres_subnet.id
   private_dns_zone_id           = var.private_dns_zone_id
   zone                          = "1"
   backup_retention_days         = 7
@@ -28,7 +49,7 @@ resource "azurerm_private_endpoint" "main" {
   name                = "pe-psql-${var.environment}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id           = var.private_endpoint_subnet_id
+  subnet_id           = azurerm_subnet.postgres_subnet.id
 
   private_service_connection {
     name                           = "psc-psql-${var.environment}"
